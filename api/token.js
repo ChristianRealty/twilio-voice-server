593af4ae2fc7bea335a4478a7190bd759167042b
ChristianRealty <co@christianortizrealty.com>
2025-04-22 12:46:36 -0400

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,26 +1,21 @@
-// api/token.js
 import twilio from 'twilio';
 
-const VoiceResponse = twilio.twiml.VoiceResponse;
-
 export default function handler(req, res) {
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
-  const clientIdentity = 'Christian'; // o el nombre que quieras para este agente
-
   const AccessToken = twilio.jwt.AccessToken;
   const VoiceGrant = AccessToken.VoiceGrant;
+
+  const token = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET,
+    { identity: 'Christian' }
+  );
 
   const voiceGrant = new VoiceGrant({
     outgoingApplicationSid: process.env.TWILIO_TWIML_APP_SID,
     incomingAllow: true
   });
 
-  const token = new AccessToken(accountSid, process.env.TWILIO_API_KEY, process.env.TWILIO_API_SECRET, {
-    identity: clientIdentity,
-  });
-
   token.addGrant(voiceGrant);
-
-  res.status(200).json(token.toJwt());
+  res.status(200).send(token.toJwt());
 }
